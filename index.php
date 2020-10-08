c31bc512b6ec272b56ddef26b3644fc45d60c753
Alexandra Ignatova <alex.ignatova@gmail.com>
2020-10-08 19:16:32 +0200

    add video-type markup

--- index.php
+++ index.php
@@ -288,6 +288,22 @@ $posts = [
                             </a>
                         </div>
                     <?php endif; ?>
+                    
+                    <?php if($post['type']=='post-video'): ?>
+                        <!--содержимое для поста-видео-->
+                        <div class="post-video__block">
+                            <div class="post-video__preview">
+                                <?=embed_youtube_cover($post['content']); ?>
+                                <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
+                            </div>
+                            <a href="post-details.html" class="post-video__play-big button">
+                                <svg class="post-video__play-big-icon" width="14" height="14">
+                                    <use xlink:href="#icon-video-play-big"></use>
+                                </svg>
+                                <span class="visually-hidden">Запустить проигрыватель</span>
+                            </a>
+                        </div>
+                    <?php endif; ?>
                 </div>
                 <footer class="post__footer">
                     <div class="post__author">
